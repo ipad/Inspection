ab21f7ab501e1dd8b380853850ba091fe2c68dbf
ipad <huzjtech@gmail.com>
2012-12-29 17:48:23 +0800

    add file
    add

---
+++ src/com/inspetion/SpecimenDetectActivity.java
@@ -0,0 +1,21 @@
+package com.inspetion;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * 
+ * @author ihzj
+ *	样本监测 Activity
+ *
+ */
+public class SpecimenDetectActivity extends Activity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		
+	}
+	
+}
