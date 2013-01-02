cb7c18a9941c4c0359a17f1e33fe6cbb0b015894
jiahong9yang <jiahong9yang@gmail.com>
2013-01-02 21:23:58 +0800

    first added

---
+++ src/com/inspetion/SysSettingActivity.java
@@ -0,0 +1,16 @@
+package com.inspetion;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SysSettingActivity extends Activity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.sys_setting_layout);
+	}
+
+}
