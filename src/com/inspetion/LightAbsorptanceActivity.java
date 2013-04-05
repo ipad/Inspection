accffc6bdc0b2a92d7e1e62264b59dab531a7685
ipad <huzjtech@gmail.com>
2013-04-06 00:50:31 +0800

    full screen.

--- src/com/inspetion/LightAbsorptanceActivity.java
+++ src/com/inspetion/LightAbsorptanceActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class LightAbsorptanceActivity extends Activity {
 
@@ -11,13 +13,22 @@ public class LightAbsorptanceActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
-		
+
+		// hide titlebar of application
+		// must be before setting the layout
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+		// hide statusbar of Android
+		// could also be done later
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+				WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
 		setContentView(R.layout.light_absorptance_testing);
 	}
-	
-	
+
 	public void back(View v) {
-		final Intent backIntent = new Intent(this,CategoryTestingActivity.class);
+		final Intent backIntent = new Intent(this,
+				CategoryTestingActivity.class);
 		startActivity(backIntent);
 	}
 }
